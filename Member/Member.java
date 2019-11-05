ac661d0bddcb0e532c83d307a79932e798f697d3
Isaac-Lee <yy0221ss@gmail.com>
2019-11-05 11:11:47 +0900

    이예성의 commit

--- Member/Member.java
+++ Member/Member.java
@@ -6,5 +6,36 @@ public class Member {
 	private String PhoneNum;
 	private String Email;
 	private boolean nowUse;
+	
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getPhoneNum() {
+		return PhoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		PhoneNum = phoneNum;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public boolean isNowUse() {
+		return nowUse;
+	}
+	public void setNowUse(boolean nowUse) {
+		this.nowUse = nowUse;
+	}
 
 }
